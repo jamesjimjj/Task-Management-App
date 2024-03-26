aeb1b41607096aa11c45279f06e43ba94bd6fe7d
jamesjimjj <115759964+jamesjimjj@users.noreply.github.com>
2024-03-26 13:54:45 -0400

    Add files via upload
    Second commit

---
+++ AccountController.cs
@@ -0,0 +1,46 @@
+﻿using Capstone_Group2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone_Group2.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // You can implement your registration logic here
+                // For simplicity, we'll just redirect to the home page
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // You can implement your login logic here
+                // For simplicity, we'll just redirect to the home page
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ TaskController.cs
@@ -0,0 +1,29 @@
+﻿using Capstone_Group2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone_Group2.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult GetAllTasks()
+        {
+            return View("Tasks");
+        }
+
+        [HttpGet]
+        public IActionResult CreateTask()
+        {
+            return View("Create");
+        }
+        [HttpPost]
+        public IActionResult CreateTask(TaskCreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
+        }
+    }
+}
